84113f970d64742bc8bd0cf04c37f0030dbe73db
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-05 04:05:13 +0300

    :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        char continueChoice;
+
+        do {
+            System.out.println("Please write your text in the following line");
+            userInput = scanner.nextLine();
+
+            String uppercasedString = userInput.toUpperCase();
+            System.out.println(uppercasedString);
+
+            System.out.println("Do you want to continue Yes or No? ");
+            continueChoice = scanner.next().charAt(0);
+            scanner.nextLine();
+
+        } while (continueChoice == 'Y' || continueChoice == 'y');
+
+        System.out.println("Bye! :)");
+        scanner.close();
     }
 }
